5177e2741aeaae2cf4fd7f348c4d8f1d2a04de40
thinksource <foretribe@gmail.com>
2017-10-09 16:50:29 +1100

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,73 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// sassThreadLoader.warmup({ workerParallelJobs: 2 }, [
+//   'sass-loader',
+//   'postcss-loader',
+//   'css-loader',
+//   'style-loader',
+//   'babel-loader',
+// ]);
 module.exports={
-  entry: './app/index.js',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    'react-hot-loader/patch'//,
+    //path.resolve(__dirname,'./app/index.js')
+  ],
   output:{
-    path:'./dist',
-    filename:'bundle.js'
+    path:path.resolve(__dirname,'./dist'),
+    filename:'bundle.js',
+    publicPath:"/assets/"
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./index.tpl.html',
+      inject:'body',
+      filename:'./index.html'
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NOOE_ENV':JSON.stringify('development')
+    })
+  ],
+  module:{
+    rules:[
+      {
+        test: /\.(js|jsx)?$/,
+        include:[
+          path.resolve(__dirname, 'app')
+        ],
+        exclude:[
+          path.resolve(__dirname,'app/demo')
+        ],
+        loader:"babel-loader",
+        options:{
+          presets:["es2015","react"]
+        },
+      },
+      // {
+      //   test:/\.html$/,
+      //   use: [
+      //     "htmllint-loader",
+      //     {
+      //       loader: "html-loader",
+      //       options: {
+      //         /* ... */
+      //       }
+      //     }
+      //   ]
+      // },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "less-loader"
+        ]
+      },
+    ]
   }
 }
